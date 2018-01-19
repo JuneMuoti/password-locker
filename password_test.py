e2acdd8815f7b81577802e68bedfaed604f0f811
June Muoti <june.muoti@gmail.com>
2018-01-19 11:40:22 +0300

    create a a untestcase class

---
+++ password_test.py
@@ -0,0 +1,9 @@
+import unittest
+from user import user
+
+class TestUser(unnittest.TestCase):
+    """
+    A test case that defines test cases for the user class
+    ""
+    Args:
+    unnittest.Testcase: TestCase class that helps in creating test cases
