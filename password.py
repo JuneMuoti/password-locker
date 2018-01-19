38c79ed401abdf837c8d4f4d73aa318b37db93cf
June Muoti <june.muoti@gmail.com>
2018-01-19 10:51:47 +0300

    create a user class

---
+++ password.py
@@ -0,0 +1,12 @@
+class User:
+    """
+    Holds all the user data
+    """
+    def __init__(self,firstname,lastname):
+        """
+        Args:
+        firstname:New User firstname
+        lastname: New User lastname
+        """
+        self.firstname= firstname
+        self.lastname = lastname
