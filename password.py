158bfeb616b898e50e9d160303b0490b4c7cd27b
June Muoti <june.muoti@gmail.com>
2018-01-19 11:27:20 +0300

    create a user list

--- password.py
+++ password.py
@@ -2,11 +2,14 @@ class User:
     """
     Holds all the user data
     """
-    def __init__(self,firstname,lastname):
+    user_list=[]
+    def __init__(self,firstname,lastname,password):
         """
         Args:
         firstname:New User firstname
         lastname: New User lastname
+        password:New User password
         """
         self.firstname= firstname
         self.lastname = lastname
+        self.password= password
