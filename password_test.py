7d1376aada0ca6e8c32db3d79aa572d4c2749e78
June Muoti <june.muoti@gmail.com>
2018-01-19 11:51:09 +0300

    define test_init and setup methods

--- password_test.py
+++ password_test.py
@@ -4,6 +4,22 @@ from user import user
 class TestUser(unnittest.TestCase):
     """
     A test case that defines test cases for the user class
-    ""
+
     Args:
-    unnittest.Testcase: TestCase class that helps in creating test cases
+    unittest.Testcase: TestCase class that helps in creating test cases
+    """
+    def setup(self):
+        """
+        Set up method to run before each test case.
+        """
+        self.new_user = User("June","Muoti","12345")
+    def test_init(self):
+        """
+        test_init case to test of the object  is initialized properly
+        """
+        self.assertEqual(self.new_user.first_name,"June")
+        self.asserEqual(self.new_user.last_name,"Muoti")
+        self.assertEqual(self.new_user.password,"12345")
+
+if __name__ == '__main__':
+    unittest.main()
